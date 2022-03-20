98ba934b68fae98fdb42af577343781a6766d5d3
jayajay25 <fujiwarasan35@gmail.com>
2022-03-20 19:03:05 +0700

    first commit

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,10 +1,19 @@
 <x-app-layout>
-    <h1>My Tasks</h1>
-    
-    <form action="/tasks/{{$task->id}}" method="post">
-        @method('put')
-        @csrf
-        <input type="text" name="list" id="list" value="{{$task->list}}" placeholder="please add task">
-        <button class="btn btn-primary" type="submit">Update</button>
-    </form>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">Update task</div>
+                    <div class="card-body">
+                        <form action="/tasks/{{$task->id}}" method="post" class="d-flex">
+                            @method('put')
+                            @csrf
+                            <input type="text" class="form-control me-2" name="list" id="list" value="{{$task->list}}">
+                            <button class="btn btn-secondary" type="submit">Update</button>
+                        </form>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>   
 </x-app-layout>
